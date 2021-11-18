a34bbe28d1c0eea9c7b1bb33e6359367ff6b7040
salimshaikh00769 <78850695+salimshaikh00769@users.noreply.github.com>
2021-11-18 14:06:28 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+let toggle = document.querySelector('.toggle');
+let menu = document.querySelector('.menu');
+
+toggle.onclick = function() {
+    menu.classList.toggle('active')
+}
+
+function takePhoto() {
+    Android.takePhoto();
+}
